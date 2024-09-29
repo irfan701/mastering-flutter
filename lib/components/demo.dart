97b33c1575c3111d60460c224e0de70203a03c15
irfan701 <irfanhossain701@gmail.com>
2024-09-29 23:08:40 +0400

    Fresh Project

---
+++ lib/components/demo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DemoW extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("This is demo page"),
+    );
+  }
+}
